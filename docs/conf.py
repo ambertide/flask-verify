55c8dbf53c0088ae75eb3530cc5f8a4eb86675e3
Ege Emir Özkan <egeemirozkan24@gmail.com>
2021-06-16 07:17:47 +0300

    Updated readme

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Ege Özkan'
 author = 'Ege Özkan'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
